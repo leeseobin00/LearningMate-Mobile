a00d22cb8a901589a217ce4147d7ea9c2dd63ad9
SeungGun <saverrw2864@naver.com>
2022-05-28 12:42:59 +0900

    add: file viewer WebView

--- app/src/main/java/com/example/learningmate/TestWebViewActivity.java
+++ app/src/main/java/com/example/learningmate/TestWebViewActivity.java
@@ -2,35 +2,47 @@ package com.example.learningmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class TestWebViewActivity extends AppCompatActivity {
-//    private String fileUrl = "http://windry.dothome.co.kr/se_learning_mate/files";
-//    private String fileName = "ec32d6ee67cc518f47dea67c4996b88b.pdf";
-    private String url = "http://windry.dothome.co.kr/se_learning_mate/files/db833060a9d1554f01d4d161b21b9fd1.pptx";
+    private final String baseViewer = "https://drive.google.com/viewerng/viewer?embedded=true&url=";
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_web_view);
-        WebView webView =findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("불러오는 중입니다...");
+        String fileUrl = "";
+        Intent intent = getIntent();
+        if (intent != null) {
+            fileUrl = intent.getStringExtra("url");
+        }
+        Log.d("url", baseViewer + fileUrl);
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressDialog.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressDialog.dismiss();
+            }
+        });
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().supportZoom();
-
-        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url="+url);
-//        webView.setWebChromeClient(new WebChromeClient(){
-//            @Override
-//            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-//                Intent intent = new Intent(Intent.ACTION_VIEW);
-//
-//            }
-//        });
-
+        webView.loadUrl(baseViewer + fileUrl);
     }
 }
