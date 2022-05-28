da25b301ff21cf61211d3cce94a192781c2a0823
SeungGun <saverrw2864@naver.com>
2022-05-28 12:43:21 +0900

    modify: splash page resolved

--- app/src/main/java/com/example/learningmate/SplashActivity.java
+++ app/src/main/java/com/example/learningmate/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.learningmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,8 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), StartActivity.class);
-        startActivity(intent);
-        finish();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 1500);
+
     }
 }
