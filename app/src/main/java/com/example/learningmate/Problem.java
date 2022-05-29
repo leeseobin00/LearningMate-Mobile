e28b8c777f7c2370bfa85f86e7270cb3a724f3bc
SeungGun <saverrw2864@naver.com>
2022-05-30 02:13:30 +0900

    feat: complete function of taking a quiz

--- app/src/main/java/com/example/learningmate/Problem.java
+++ app/src/main/java/com/example/learningmate/Problem.java
@@ -1,17 +1,95 @@
 package com.example.learningmate;
 
 public class Problem {
-    public String problem;
-    public String answer1;
-    public String answer2;
-    public String answer3;
-    public String answer4;
+    private String problem;
+    private String choice1;
+    private String choice2;
+    private String choice3;
+    private String choice4;
+    private int point;
+    private String answer;
+    private String selectAnswer;
+    private int selectedId;
 
-    public Problem(String problem, String answer1, String answer2, String answer3, String answer4){
+    public Problem(String problem, String choice1, String choice2, String choice3, String choice4, int point, String answer) {
         this.problem = problem;
-        this.answer1 = answer1;
-        this.answer2 = answer2;
-        this.answer3 = answer3;
-        this.answer4 = answer4;
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+        this.choice3 = choice3;
+        this.choice4 = choice4;
+        this.point = point;
+        this.answer = answer;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+
+    public String getChoice1() {
+        return choice1;
+    }
+
+    public void setChoice1(String choice1) {
+        this.choice1 = choice1;
+    }
+
+    public String getChoice2() {
+        return choice2;
+    }
+
+    public void setChoice2(String choice2) {
+        this.choice2 = choice2;
+    }
+
+    public String getChoice3() {
+        return choice3;
+    }
+
+    public void setChoice3(String choice3) {
+        this.choice3 = choice3;
+    }
+
+    public String getChoice4() {
+        return choice4;
+    }
+
+    public void setChoice4(String choice4) {
+        this.choice4 = choice4;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getSelectAnswer() {
+        return selectAnswer;
+    }
+
+    public void setSelectAnswer(String selectAnswer) {
+        this.selectAnswer = selectAnswer;
+    }
+
+    public int getSelectedId() {
+        return selectedId;
+    }
+
+    public void setSelectedId(int selectedId) {
+        this.selectedId = selectedId;
     }
 }
--- app/src/main/java/com/example/learningmate/ProblemActivity.java
+++ app/src/main/java/com/example/learningmate/ProblemActivity.java
@@ -2,13 +2,29 @@ package com.example.learningmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import java.util.ArrayList;
 
@@ -16,46 +32,226 @@ public class ProblemActivity extends AppCompatActivity {
 
     RecyclerView problemRecyclerView;
     RecyclerView.LayoutManager layoutManager;
-
+    private ArrayList<Problem> problemArrayList;
     private Button btn_submit;
+    private Quiz quiz;
+    private ProblemRVAdapter problemRVAdapter;
+    private Button startButton;
+    private int startTime = 0;
+    private TextView timer;
+    private Handler handler = new Handler(Looper.myLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.arg1 == 2) {
+                Toast.makeText(ProblemActivity.this, "퀴즈가 종료되었습니다.", Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+            if (msg.arg2 == 1) {
+                problemRVAdapter = new ProblemRVAdapter(problemArrayList);
+                problemRecyclerView.setAdapter(problemRVAdapter);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
-
-        problemRecyclerView  = findViewById(R.id.problem_rv);
+        Intent intent = getIntent();
+        if (intent != null) {
+            quiz = (Quiz) intent.getSerializableExtra("data");
+        }
+        startButton = findViewById(R.id.start_button);
+        problemRecyclerView = findViewById(R.id.problem_rv);
         problemRecyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         problemRecyclerView.setLayoutManager(layoutManager);
-
-        ArrayList<Problem> problemArrayList = new ArrayList<>();
-
-        problemArrayList.add(new Problem("문제1 :", "1","2","3","4"));
-        problemArrayList.add(new Problem("문제2 :", "1","2","3","4"));
-        problemArrayList.add(new Problem("문제3 :", "1","2","3","4"));
-        problemArrayList.add(new Problem("문제4 :", "1","2","3","4"));
-
-        ProblemRVAdapter problemRVAdapter = new ProblemRVAdapter(problemArrayList);
-        problemRecyclerView.setAdapter(problemRVAdapter);
-
+        problemArrayList = new ArrayList<>();
         btn_submit = findViewById(R.id.quizSubmit);
+        timer = findViewById(R.id.timer_tv);
+        startButton.setText("시작하기 [제한시간: "+quiz.getTimeLimit()+"초]");
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startButton.setVisibility(View.GONE);
+                problemRecyclerView.setVisibility(View.VISIBLE);
+                btn_submit.setVisibility(View.VISIBLE);
+                startTime = quiz.getTimeLimit();
+                timer.setText(startTime + "");
+                timer.setVisibility(View.VISIBLE);
+                executeTimer();
+            }
+        });
+
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // DB에 저장된 정답과 멘티가 입력한 정답과 비교하여 점수 계산
-
                 // 퀴즈 목록으로 이동함
-                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
-                startActivity(intent);
+                final int grade = calculateGrade();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateQuizScore(grade);
+                    }
+                }).start();
             }
         });
-        ImageButton back_ib = findViewById(R.id.back_ib);
-        back_ib.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.back_ib).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                finish();
             }
         });
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getQuizList();
+            }
+        }).start();
+    }
+
+    private int calculateGrade() {
+        int grade = 0;
+        for (int i = 0; i < problemArrayList.size(); ++i) {
+            if (problemArrayList.get(i).getAnswer().equals(problemArrayList.get(i).getSelectAnswer())) {
+                grade += problemArrayList.get(i).getPoint();
+            }
+        }
+        return grade;
+    }
+
+    public void getQuizList() {
+        try {
+            String url = "http://windry.dothome.co.kr/se_learning_mate/controller/quiz_controller.php";
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("quiz_id_detail", quiz.getQuizId())
+                    .build();
+
+            Request request = new Request
+                    .Builder()
+                    .url(url)
+                    .post(formBody).build();
+
+            Response response = client
+                    .newCall(request)
+                    .execute();
+
+            if (response.isSuccessful()) {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String data = body.string();
+                    Log.d("data", data);
+                    if (data.equals("None")) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ProblemActivity.this, "퀴즈가 없습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        JSONArray jsonArray = new JSONArray(data);
+                        for (int i = 0; i < jsonArray.length(); ++i) {
+                            JSONObject jsonObject = new JSONObject(jsonArray.getString(i));
+                            problemArrayList.add(new Problem(jsonObject.optString("question")
+                                    , jsonObject.optString("choice1")
+                                    , jsonObject.optString("choice2")
+                                    , jsonObject.optString("choice3")
+                                    , jsonObject.optString("choice4")
+                                    , Integer.parseInt(jsonObject.optString("point"))
+                                    , jsonObject.optString("answer")));
+                        }
+                        Message message = handler.obtainMessage();
+                        message.arg2 = 1;
+                        handler.sendMessage(message);
+                    }
+                }
+            } else {
+                Log.d("response", "error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateQuizScore(int score) {
+        try {
+            String url = "http://windry.dothome.co.kr/se_learning_mate/controller/quiz_controller.php";
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("update_grade_quiz_id", quiz.getQuizId())
+                    .add("update_grade_score", score + "")
+                    .build();
+
+            Request request = new Request
+                    .Builder()
+                    .url(url)
+                    .post(formBody).build();
+
+            Response response = client
+                    .newCall(request)
+                    .execute();
+
+            if (response.isSuccessful()) {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String data = body.string();
+                    Log.d("data", data);
+                    if (data.equals("0")) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ProblemActivity.this, "점수 반영 실패, 재시도 바람.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        Message message = handler.obtainMessage();
+                        message.arg1 = 2;
+                        handler.sendMessage(message);
+                    }
+                }
+            } else {
+                Log.d("response", "error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executeTimer() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (startTime != 0) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            startTime--;
+                            timer.setText("남은시간: " + startTime + "초");
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        startTime = 0;
+                    }
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        final int grade = calculateGrade();
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateQuizScore(grade);
+                            }
+                        }).start();
+                    }
+                });
+            }
+        }).start();
     }
 }
