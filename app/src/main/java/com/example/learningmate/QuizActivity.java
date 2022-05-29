e90b59468e394ee96e95c9b8e1e6eecfcee09fa8
SeungGun <saverrw2864@naver.com>
2022-05-30 02:11:01 +0900

    add: get quiz data list in quiz main page

--- app/src/main/java/com/example/learningmate/QuizActivity.java
+++ app/src/main/java/com/example/learningmate/QuizActivity.java
@@ -2,54 +2,143 @@ package com.example.learningmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import java.util.ArrayList;
 
 public class QuizActivity extends AppCompatActivity {
 
-    RecyclerView quizRecyclerViews;
-    RecyclerView.LayoutManager layoutManager;
+    private RecyclerView quizRecyclerView;
+    private ArrayList<Quiz> quizArrayList;
+    private QuizRVAdapter quizRVAdapter;
+    private Handler handler = new Handler(Looper.myLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.arg2 == 1) {
+                quizRVAdapter = new QuizRVAdapter(quizArrayList);
+                quizRecyclerView.setAdapter(quizRVAdapter);
+                quizRVAdapter.setOnItemClickListener(new QuizRVAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View v, int position) {
+                        if (quizArrayList.get(position).getQuizGrade() == -1) {
+                            Intent intent = new Intent(getApplicationContext(), ProblemActivity.class);
+                            intent.putExtra("data", quizArrayList.get(position));
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(QuizActivity.this, "이미 응시한 퀴즈입니다!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        quizRecyclerView = findViewById(R.id.quiz_rv);
+        quizRecyclerView.setHasFixedSize(true);
+        quizRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        quizArrayList = new ArrayList<>();
 
-        quizRecyclerViews = findViewById(R.id.quiz_rv);
-        quizRecyclerViews.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        quizRecyclerViews.setLayoutManager(layoutManager);
-
-        ArrayList<Quiz> quizArrayList = new ArrayList<>();
-
-        quizArrayList.add(new Quiz("퀴즈1"));
-        quizArrayList.add(new Quiz("퀴즈2"));
-        quizArrayList.add(new Quiz("퀴즈3"));
-
-        QuizRVAdapter quizRVAdapter = new QuizRVAdapter(quizArrayList);
-        quizRecyclerViews.setAdapter(quizRVAdapter);
-
-        quizRVAdapter.setOnItemClickListener(new QuizRVAdapter.OnItemClickListener(){
+        findViewById(R.id.back_ib).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(View v, int position) {
-                Intent intent = new Intent(getApplicationContext(), ProblemActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                finish();
             }
         });
-        ImageButton back_ib = findViewById(R.id.back_ib);
-        back_ib.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            public void run() {
+                getQuizList();
             }
-        });
+        }).start();
 
+    }
+
+    public void getQuizList() {
+        try {
+            String url = "http://windry.dothome.co.kr/se_learning_mate/controller/quiz_controller.php";
+            OkHttpClient client = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("get_quiz_uid", User.currentUser.getPairId())
+                    .build();
+
+            Request request = new Request
+                    .Builder()
+                    .url(url)
+                    .post(formBody).build();
+
+            Response response = client
+                    .newCall(request)
+                    .execute();
+
+            if (response.isSuccessful()) {
+                ResponseBody body = response.body();
+                if (body != null) {
+                    String data = body.string();
+                    Log.d("data", data);
+                    if (data.equals("None")) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(QuizActivity.this, "퀴즈가 없습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        quizArrayList.clear();
+                        JSONArray jsonArray = new JSONArray(data);
+                        for (int i = 0; i < jsonArray.length(); ++i) {
+                            JSONObject jsonObject = new JSONObject(jsonArray.getString(i));
+                            quizArrayList.add(new Quiz(jsonObject.optString("quiz_id")
+                                    , jsonObject.optString("quiz_owner")
+                                    , jsonObject.optString("title")
+                                    , Integer.parseInt(jsonObject.optString("time_limit"))
+                                    , jsonObject.optString("quiz_grade").equals("null") ? -1 : Integer.parseInt(jsonObject.optString("quiz_grade"))
+                                    , Integer.parseInt(jsonObject.optString("perfect_score"))));
+                        }
+                        Message message = handler.obtainMessage();
+                        message.arg2 = 1;
+                        handler.sendMessage(message);
+                    }
+                }
+            } else {
+                Log.d("response", "error");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getQuizList();
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/example/learningmate/QuizRVAdapter.java
+++ app/src/main/java/com/example/learningmate/QuizRVAdapter.java
@@ -13,30 +13,35 @@ import java.util.ArrayList;
 public class QuizRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     //퀴즈 아이템 누르면 problemActivity로 전환하기 위하여 필요한 것
-    interface OnItemClickListener{
+    interface OnItemClickListener {
         void onItemClick(View v, int position);
     }
+
     private OnItemClickListener mListener = null;
-    public void setOnItemClickListener(OnItemClickListener listener){
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
         this.mListener = listener;
     }
 
 
-    public  class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView quiz_name_tv;
+        TextView score;
+        TextView totalScore;
 
-        MyViewHolder(View view){
+        MyViewHolder(View view) {
             super(view);
             quiz_name_tv = view.findViewById(R.id.quiz_name_tv);
-
+            score = view.findViewById(R.id.score_tv);
+            totalScore = view.findViewById(R.id.totalscore_tv);
             //퀴즈 아이템 누르면 problemActivity로 전환하기 위하여 필요한 것
-            itemView.setOnClickListener(new View.OnClickListener(){
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view){
-                    int position = getAdapterPosition ();
-                    if(position!=RecyclerView.NO_POSITION){
-                        if(mListener!=null){
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        if (mListener != null) {
                             mListener.onItemClick(view, position);
                         }
                     }
@@ -47,14 +52,15 @@ public class QuizRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
 
     private ArrayList<Quiz> quizArrayList;
+
     QuizRVAdapter(ArrayList<Quiz> list) {
         this.quizArrayList = list;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_quiz,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_quiz, parent, false);
         return new MyViewHolder(view);
     }
 
@@ -62,7 +68,9 @@ public class QuizRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         MyViewHolder myViewHolder = (MyViewHolder) holder;
 
-        myViewHolder.quiz_name_tv.setText(quizArrayList.get(position).quizName);
+        myViewHolder.quiz_name_tv.setText(quizArrayList.get(position).getTitle());
+        myViewHolder.score.setText(quizArrayList.get(position).getQuizGrade() == -1 ? "-" : quizArrayList.get(position).getQuizGrade() + "");
+        myViewHolder.totalScore.setText(quizArrayList.get(position).getPerfectScore() + "");
     }
 
 
