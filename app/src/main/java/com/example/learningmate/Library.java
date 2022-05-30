daea0de47f9f7a5ddbdaf84dffaeea51e64077de
SeungGun <saverrw2864@naver.com>
2022-05-30 15:19:13 +0900

    modify: attributes of library model class

--- app/src/main/java/com/example/learningmate/Library.java
+++ app/src/main/java/com/example/learningmate/Library.java
@@ -1,28 +1,47 @@
 package com.example.learningmate;
 
 public class Library {
-    String libraryName;
-    String fileName;
+    private String title;
+    private String fileName;
+    private String fileUrl;
+    private String uploadDate;
 
-    public Library(String libraryName, String fileName){
-        this.libraryName = libraryName;
+    public Library(String title, String fileName, String fileUrl) {
+        this.title = title;
         this.fileName = fileName;
+        this.fileUrl = fileUrl;
     }
 
-    public String getLibraryNamey() {
-        return libraryName;
+    public String getTitle() {
+        return title;
     }
 
-    public void setLibraryName(String libraryName) {
-        this.libraryName = libraryName;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getFileName() {
         return fileName;
     }
 
-    public void setFileName(String libraryName) {
+    public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(String uploadDate) {
+        this.uploadDate = uploadDate;
     }
 }
 
