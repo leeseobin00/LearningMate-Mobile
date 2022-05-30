376bf5866b8df7cb9ad393ace251d936eb715a31
SeungGun <saverrw2864@naver.com>
2022-05-30 19:52:23 +0900

    modify: complete function with layout in alert of mentor

--- app/src/main/java/com/example/learningmate/AlertMentor.java
+++ app/src/main/java/com/example/learningmate/AlertMentor.java
@@ -2,10 +2,26 @@ package com.example.learningmate;
 
 public class AlertMentor {
     public String alertName;
-    public String state;
+    public String date;
 
-    public AlertMentor(String alertName, String state){
+    public AlertMentor(String alertName, String date){
         this.alertName = alertName;
-        this.state = state;
+        this.date = date;
+    }
+
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public void setAlertName(String alertName) {
+        this.alertName = alertName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
