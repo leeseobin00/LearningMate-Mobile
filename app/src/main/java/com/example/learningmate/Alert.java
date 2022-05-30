d07d73f5a502321ad66d5926a2b16482e8d3dad5
SeungGun <saverrw2864@naver.com>
2022-05-30 19:51:11 +0900

    modify: attribute of alert model class

--- app/src/main/java/com/example/learningmate/Alert.java
+++ app/src/main/java/com/example/learningmate/Alert.java
@@ -1,17 +1,63 @@
 package com.example.learningmate;
 
 public class Alert {
-    public String alertName;
-    public int year;
-    public int month;
-    public int day;
-    public String state;
+    private String alertName;
+    private String uploadDate;
+    private String dueDate;
+    private boolean state;
+    private int viewType;
 
-    public Alert(String alertName, int year, int month, int day, String state){
+    public Alert(String alertName, String uploadDate, String dueDate, boolean state, int viewType) {
         this.alertName = alertName;
-        this.year = year;
-        this.month = month;
-        this.day = day;
+        this.uploadDate = uploadDate;
+        this.dueDate = dueDate;
         this.state = state;
+        this.viewType = viewType;
+    }
+
+    public Alert(String alertName, String uploadDate, int viewType){
+        this.alertName = alertName;
+        this.uploadDate = uploadDate;
+        this.viewType = viewType;
+    }
+
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public void setAlertName(String alertName) {
+        this.alertName = alertName;
+    }
+
+    public String getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(String uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public int getViewType() {
+        return viewType;
+    }
+
+    public void setViewType(int viewType) {
+        this.viewType = viewType;
     }
 }
